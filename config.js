860f183f00d01e217f8759a74c9a01b34182f8c3
santiyt65 <santiloez6@gmail.com>
2024-08-16 03:20:54 -0300

    TANJIRO-BOT
    TANJIRO-BOT

--- config.js
+++ config.js
@@ -31,7 +31,6 @@ global.imagen1 = fs.readFileSync('./Menu2.jpg');
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
 global.imagen4 = fs.readFileSync('./Menu.png');
-global.imagen5 = fs.readFileSync('./src/+18.jpg');
 global.imagen6 = fs.readFileSync('./Menu3.png');
 global.imagen7 = fs.readFileSync('./src/menu_pt.png');
 global.imagen8 = fs.readFileSync('./src/menu_fr.png')
